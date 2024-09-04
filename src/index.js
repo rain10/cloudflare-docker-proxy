288bedba39961aeb8e54bc27c6a516fa6cc8b3ef
rain10 <2530728915@qq.com>
2024-09-04 16:38:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zhaoyuliu.top": dockerHub,
+  "quay.zhaoyuliu.top": "https://quay.io",
+  "gcr.zhaoyuliu.top": "https://gcr.io",
+  "k8s-gcr.zhaoyuliu.top": "https://k8s.gcr.io",
+  "k8s.zhaoyuliu.top": "https://registry.k8s.io",
+  "ghcr.zhaoyuliu.top": "https://ghcr.io",
+  "cloudsmith.zhaoyuliu.top": "https://docker.cloudsmith.io",
+  "ecr.zhaoyuliu.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zhaoyuliu.top": dockerHub,
 };
 
 function routeByHosts(host) {
